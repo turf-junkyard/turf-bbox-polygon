03f2a85ba0e4c007485e1604111618947233b980
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-08 22:47:46 -0400

    first pass refactor

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var t = {}
-var point = require('../lib/point'),
-    polygon = require('../lib/polygon')
-t.point = point
-t.polygon = polygon
+var polygon = require('turf-polygon')
 
-module.exports = function(bbox, done){
-  var lowLeft = t.point(bbox[0], bbox[1])
-  var topLeft = t.point(bbox[0], bbox[3])
-  var topRight = t.point(bbox[2], bbox[3])
-  var lowRight = t.point(bbox[2], bbox[1])
+module.exports = function(bbox){
+  var lowLeft = [bbox[0], bbox[1]]
+  var topLeft = [bbox[0], bbox[3]]
+  var topRight = [bbox[2], bbox[3]]
+  var lowRight = [bbox[2], bbox[1]]
 
-  var poly = t.polygon([[
-    lowLeft.geometry.coordinates, 
-    lowRight.geometry.coordinates, 
-    topRight.geometry.coordinates, 
-    topLeft.geometry.coordinates
+  var poly = polygon([[
+    lowLeft,
+    lowRight,
+    topRight,
+    topLeft
   ]])
-  done(null, poly)
+  return poly
 }
