c7b98eba4d5426e4bc11b82e9a4014683f3ce9e4
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-08 22:52:36 -0400

    release

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ test('bboxPolygon', function(t){
   var poly = bboxPolygon([0,0,10,10])
 
   t.ok(poly.geometry.coordinates, 'should take a bbox and return the equivalent polygon feature')
-  t.equal(poly.geometry.type, 'Polygon', 'should be a Polygon geometry')
+  t.equal(poly.geometry.type, 'Polygon', 'should be a Polygon geometry type')
 })
