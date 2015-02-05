db91b21e237c7c3ad8a68d8a71f3302710e24f28
Tom MacWright <tom@macwright.org>
2015-02-05 15:17:50 -0500

    Add category

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var polygon = require('turf-polygon');
 
 /**
-* Takes a bbox and returns the equivalent {@link Polygon} feature.
-*
-* @module turf/bbox-polygon
-* @param {Array<number>} bbox an Array of bounding box coordinates in the form: ```[xLow, yLow, xHigh, yHigh]```
-* @return {Polygon} a Polygon representation of the bounding box
-* @example
-* var bbox = [0, 0, 10, 10];
-*
-* var poly = turf.bboxPolygon(bbox);
-*
-* //=poly
-*/
+ * Takes a bbox and returns the equivalent {@link Polygon} feature.
+ *
+ * @module turf/bbox-polygon
+ * @category measurement
+ * @param {Array<number>} bbox an Array of bounding box coordinates in the form: ```[xLow, yLow, xHigh, yHigh]```
+ * @return {Polygon} a Polygon representation of the bounding box
+ * @example
+ * var bbox = [0, 0, 10, 10];
+ *
+ * var poly = turf.bboxPolygon(bbox);
+ *
+ * //=poly
+ */
 
 module.exports = function(bbox){
   var lowLeft = [bbox[0], bbox[1]];
